47cafec20721d467961a30b34668e4f76c9b5803
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-11-19 19:22:17 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
+const SiteController = require ("./controllers/SiteController.js")
+const PhotosController = require ("./controllers/SiteController.js")
 
 
 // Routes
-
+app.get("/", SiteController.Index)
+app.get("/about", SiteController.About)
+app.get("/contact", SiteController.Contact)
+app.get("/photos", PhotosController.Index)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
